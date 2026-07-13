a5e6eb5965f2619f6be75b7cc65d1b911277f1c8
Aditya Sharma <adityakushinagar123@gmail.com>
2026-07-13 12:46:23 +0530

    a

--- leetcode-pattern/sliding-window/tempCodeRunnerFile.js
+++ leetcode-pattern/sliding-window/tempCodeRunnerFile.js
@@ -1,2 +1,27 @@
-    // console.log("idx",idx)
-            // console.log("i",i)
+function largestSub(arr, target) {
+//   let i = 0;
+//   let j = 0;
+//   let sum = 0;
+//   let ans = -Infinity;
+//   while (j < arr.length) {
+//     sum += arr[j];
+//     if (sum < target) {
+//       j++;
+//     } else if (sum === target) {
+//       let size = j - i + 1;
+//       console.log("size",size)
+//       ans = Math.max(size, ans);
+//       j++;
+//     } else {
+//         if(sum > target){
+//             while(sum > target){
+//             sum-=arr[i];
+//             i++;
+//         }
+//         }
+//           j++;
+//     }
+  
+//   }
+//   return ans;
+// }
