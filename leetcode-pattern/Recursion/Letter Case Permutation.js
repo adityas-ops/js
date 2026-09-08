adc9f3ada13004497eb0264b7d711727feb6986e
Aditya Sharma <adityakushinagar123@gmail.com>
2026-09-08 17:54:30 +0530

    a

--- leetcode-pattern/Recursion/Letter Case Permutation.js
+++ leetcode-pattern/Recursion/Letter Case Permutation.js
@@ -2,12 +2,9 @@ let ip = "a1B2";
 // output "a1B2", A1B2, A1b2, A1B2
 let op = "";
 
-function isNumber(char) {
-  return char >= '0' && char <= '9';
+function isNumber(ch) {
+  return ch >= "0" && ch <= "9";
 }
-
-console.log(isNumber('a'))
-
 
 function toggleCase(char) {
   if (char === char.toUpperCase()) {
@@ -15,10 +12,10 @@ function toggleCase(char) {
   } else if (char === char.toLowerCase()) {
     return char.toUpperCase();
   }
-  return char; 
+  return char;
 }
 
-console.log('a',toggleCase('5'))
+// console.log('a',toggleCase('5'))
 
 function solve(ip, op) {
   if (ip.length === 0) {
@@ -28,6 +25,18 @@ function solve(ip, op) {
 
   let op1 = op;
   let op2 = op;
-  let temp = toggleCase(ip[0])
-  
+
+  if (isNumber(ip[0])) {
+    solve(ip.substring(1, ip.length), op + ip[0]);
+    return;
+  } else {
+    let temp = toggleCase(ip[0]);
+    op1 += temp;
+    op2 += ip[0];
+  }
+  ip = ip.substring(1, ip.length);
+  solve(ip, op1);
+  solve(ip, op2);
 }
+
+solve(ip, op);
